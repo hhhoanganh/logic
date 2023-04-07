34a796e574d3e6a63ea215ae7d7855fe9e976661
hhhoanganh <101188848+hhhoanganh@users.noreply.github.com>
2023-04-07 20:17:47 +0700

    Add files via upload

--- countCharater.java
+++ countCharater.java
@@ -1,27 +1,18 @@
-import java.util.ArrayList;
-
+import java.util.HashMap;
 public class countCharater {
-    public static void main(String[] args) {
-        String str="afdjkjwkmvooiwmnclsjujdkjdsksl";
-        ArrayList<Character> str1=new ArrayList<>();
-        ArrayList<Integer> str2 = new ArrayList<>();
-        while(!str.isEmpty()){
-            char val=str.charAt(0);
-            int isNull = 0;
-            for(int i=0;i<str1.size();i++){
-                if(str1.get(i)==val){
-                    isNull=1;
-                    str2.set(i, str2.get(i)+1);
-                }
+    static void characterCount(String inputString) {
+        HashMap<Character, Integer> cha = new HashMap<Character, Integer>();
+        char[] charArray = inputString.toCharArray();
+        for (char c : charArray) {
+            if(cha.containsKey(c)) {
+                cha.put(c, cha.get(c)+1);
+            } else {
+                cha.put(c, 1);
             }
-            if(isNull==0){
-                str1.add(val);
-                str2.add(1);
-            }
-            isNull=0;
-            str=str.substring(1);
         }
-        System.out.println(str1);
-        System.out.println(str2);
+        System.out.println(cha);
+    }
+    public static void main(String[] args) {
+        characterCount("snfjknajnqdankjasmaslaosdk");
     }
 }
